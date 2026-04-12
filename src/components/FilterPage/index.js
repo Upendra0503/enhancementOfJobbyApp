3598c49c0ade648e3e7a676316b9dc7fc18bc823
Upendra <kumaruppi788@gmail.com>
2026-04-12 03:28:00 +0000

    Initial commit - Restaurant/Jobs project

--- src/components/FilterPage/index.js
+++ src/components/FilterPage/index.js
@@ -1,55 +1,62 @@
 import './index.css'
 
 const FilterPage = props => {
-  const {employee, salary, changeEmploymentType, changeSalaryRange} = props
+  const {
+    employee,
+    salary,
+    changeEmploymentType,
+    changeSalaryRange,
+    changeLocation,
+  } = props
 
-  const changeEmployee = event => {
-    changeEmploymentType(event.target.value)
-  }
-
-  const changeSalary = event => {
-    changeSalaryRange(event.target.value)
-  }
-
-  const renderEmployeeDetails = () =>
-    employee.map(each => (
-      <li key={each.employmentTypeId}>
-        <input
-          type="checkbox"
-          id={each.employmentTypeId}
-          onChange={changeEmployee}
-          value={each.employmentTypeId}
-        />
-        <label className="labelEmployee" htmlFor={each.employmentTypeId}>
-          {each.label}
-        </label>
-      </li>
-    ))
-
-  const renderSalaryRanges = () =>
-    salary.map(each => (
-      <li key={each.salaryRangeId}>
-        <input
-          type="radio"
-          id={each.salaryRangeId}
-          name="salary"
-          value={each.salaryRangeId}
-          onChange={changeSalary}
-        />
-        <label className="labelSalary" htmlFor={each.salaryRangeId}>
-          {each.label}
-        </label>
-      </li>
-    ))
+  const locationsList = [
+    {label: 'Hyderabad', id: 'HYDERABAD'},
+    {label: 'Bangalore', id: 'BANGALORE'},
+    {label: 'Chennai', id: 'CHENNAI'},
+    {label: 'Delhi', id: 'DELHI'},
+    {label: 'Mumbai', id: 'MUMBAI'},
+  ]
 
   return (
     <div>
-      <h1>Type of Employment</h1>
-      <ul>{renderEmployeeDetails()}</ul>
-      <hr />
+      <h1 className="typeH1">Type of Employment</h1>
+      {employee.map(each => (
+        <div key={each.employmentTypeId}>
+          <input
+            type="checkbox"
+            value={each.employmentTypeId}
+            onChange={e => changeEmploymentType(e.target.value)}
+          />
+          <label className="labelEmployee">{each.label}</label>
+        </div>
+      ))}
+
       <h1>Salary Range</h1>
-      <ul>{renderSalaryRanges()}</ul>
+      {salary.map(each => (
+        <div key={each.salaryRangeId}>
+          <input
+            type="radio"
+            name="salary"
+            value={each.salaryRangeId}
+            onChange={e => changeSalaryRange(e.target.value)}
+          />
+          <label className="labelSalary">{each.label}</label>
+        </div>
+      ))}
+
+      <h1>Locations</h1>
+      {locationsList.map(each => (
+        <div key={each.id}>
+          <input
+            type="checkbox"
+            value={each.id}
+            onChange={e => changeLocation(e.target.value)}
+          />
+          <label className="labelLocation">{each.label}</label>
+        </div>
+      ))}
     </div>
   )
 }
+
 export default FilterPage
--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -9,41 +9,17 @@ import JobItemDetails from '../JobItemDetails'
 import './index.css'
 
 const employmentTypesList = [
-  {
-    label: 'Full Time',
-    employmentTypeId: 'FULLTIME',
-  },
-  {
-    label: 'Part Time',
-    employmentTypeId: 'PARTTIME',
-  },
-  {
-    label: 'Freelance',
-    employmentTypeId: 'FREELANCE',
-  },
-  {
-    label: 'Internship',
-    employmentTypeId: 'INTERNSHIP',
-  },
+  {label: 'Full Time', employmentTypeId: 'FULLTIME'},
+  {label: 'Part Time', employmentTypeId: 'PARTTIME'},
+  {label: 'Freelance', employmentTypeId: 'FREELANCE'},
+  {label: 'Internship', employmentTypeId: 'INTERNSHIP'},
 ]
 
 const salaryRangesList = [
-  {
-    salaryRangeId: '1000000',
-    label: '10 LPA and above',
-  },
-  {
-    salaryRangeId: '2000000',
-    label: '20 LPA and above',
-  },
-  {
-    salaryRangeId: '3000000',
-    label: '30 LPA and above',
-  },
-  {
-    salaryRangeId: '4000000',
-    label: '40 LPA and above',
-  },
+  {salaryRangeId: '1000000', label: '10 LPA and above'},
+  {salaryRangeId: '2000000', label: '20 LPA and above'},
+  {salaryRangeId: '3000000', label: '30 LPA and above'},
+  {salaryRangeId: '4000000', label: '40 LPA and above'},
 ]
 
 const apiStatus = {
@@ -59,6 +35,7 @@ class Jobs extends Component {
     searchInput: '',
     employmentTypes: [],
     salaryRange: '',
+    locations: [],
   }
 
   componentDidMount() {
@@ -67,18 +44,24 @@ class Jobs extends Component {
 
   getJobDetails = async () => {
     this.setState({status: apiStatus.inProgress})
+
     const {salaryRange, searchInput, employmentTypes} = this.state
     const employmentTypeQuery = employmentTypes.join(',')
+
     const url = `https://apis.ccbp.in/jobs?employment_type=${employmentTypeQuery}&minimum_package=${salaryRange}&search=${searchInput}`
+
     const jwtToken = Cookies.get('jwt_token')
+
     const options = {
       method: 'GET',
       headers: {
         Authorization: `Bearer ${jwtToken}`,
       },
     }
+
     const response = await fetch(url, options)
     const data = await response.json()
+
     if (response.ok === true) {
       const updatedData = data.jobs.map(each => ({
         companyLogoUrl: each.company_logo_url,
@@ -90,34 +73,11 @@ class Jobs extends Component {
         rating: each.rating,
         title: each.title,
       }))
+
       this.setState({jobsData: updatedData, status: apiStatus.success})
     } else {
       this.setState({status: apiStatus.failure})
     }
-  }
-
-  getRenderFailure = () => (
-    <div>
-      <img
-        alt="failure view"
-        src="https://assets.ccbp.in/frontend/react-js/failure-img.png"
-      />
-      <h1>Oops! Something Went Wrong</h1>
-      <p>We cannot seem to find the page you are looking for.</p>
-      <button type="button" onClick={this.getJobDetails}>
-        Retry
-      </button>
-    </div>
-  )
-
-  getRenderInProgress = () => (
-    <div className="loader-container" data-testid="loader">
-      <Loader type="ThreeDots" color="#ffffff" height="50" width="50" />
-    </div>
-  )
-
-  changeSalaryRange = id => {
-    this.setState({salaryRange: id}, this.getJobDetails)
   }
 
   changeEmploymentType = id => {
@@ -131,6 +91,18 @@ class Jobs extends Component {
     )
   }
 
+  changeSalaryRange = id => {
+    this.setState({salaryRange: id}, this.getJobDetails)
+  }
+
+  changeLocation = id => {
+    this.setState(prevState => ({
+      locations: prevState.locations.includes(id)
+        ? prevState.locations.filter(each => each !== id)
+        : [...prevState.locations, id],
+    }))
+  }
+
   searchData = event => {
     this.setState({searchInput: event.target.value})
   }
@@ -139,9 +111,41 @@ class Jobs extends Component {
     this.getJobDetails()
   }
 
+  getFilteredJobs = () => {
+    const {
+      jobsData,
+      employmentTypes,
+      salaryRange,
+      locations,
+      searchInput,
+    } = this.state
+
+    return jobsData.filter(job => {
+      const matchEmployment =
+        employmentTypes.length === 0 ||
+        employmentTypes.includes(job.employmentType)
+
+      const matchSalary =
+        salaryRange === '' || Number(job.packagePerAnnum) >= Number(salaryRange)
+
+      const matchLocation =
+        locations.length === 0 ||
+        locations.some(
+          each => each.toLowerCase() === job.location.toLowerCase(),
+        )
+
+      const matchSearch = job.title
+        .toLowerCase()
+        .includes(searchInput.toLowerCase())
+
+      return matchEmployment && matchSalary && matchLocation && matchSearch
+    })
+  }
+
   getRenderSuccess = () => {
-    const {jobsData} = this.state
-    if (jobsData.length === 0) {
+    const filteredJobs = this.getFilteredJobs()
+
+    if (filteredJobs.length === 0) {
       return (
         <div className="noDataCont">
           <img
@@ -149,39 +153,28 @@ class Jobs extends Component {
             src="https://assets.ccbp.in/frontend/react-js/no-jobs-img.png"
           />
           <h1>No Jobs Found</h1>
-          <p>We could not find any jobs. Try other filters.</p>
         </div>
       )
     }
+
     return (
       <ul className="jobsUl">
-        {jobsData.map(each => (
+        {filteredJobs.map(each => (
           <JobItemDetails key={each.id} each={each} />
         ))}
       </ul>
     )
   }
 
-  getSwitchstatement = () => {
-    const {status} = this.state
-    switch (status) {
-      case apiStatus.success:
-        return this.getRenderSuccess()
-      case apiStatus.failure:
-        return this.getRenderFailure()
-      case apiStatus.inProgress:
-        return this.getRenderInProgress()
-      default:
-        return null
-    }
-  }
-
   render() {
-    const {searchInput} = this.state
+    const {searchInput, status} = this.state
+
     return (
       <div className="jobsCont">
         <Header />
+
         <div className="jobsBottomCont">
+          {/* LEFT SIDE FILTER */}
           <div className="jobsBottomLeftCont">
             <Profile />
             <hr />
@@ -190,30 +183,33 @@ class Jobs extends Component {
               salary={salaryRangesList}
               changeEmploymentType={this.changeEmploymentType}
               changeSalaryRange={this.changeSalaryRange}
+              changeLocation={this.changeLocation}
             />
           </div>
+
           <div className="jobsBottomRightCont">
             <div className="jobBottomInputCont">
               <input
                 placeholder="search"
                 type="search"
-                className="jobInputCont"
-                onChange={this.searchData}
                 value={searchInput}
+                onChange={this.searchData}
               />
-              <button
-                type="button"
-                onClick={this.onSearch}
-                data-testid="searchButton"
-              >
-                <BsSearch className="search-icon" />
+              <button type="button" onClick={this.onSearch}>
+                <BsSearch />
               </button>
             </div>
-            {this.getSwitchstatement()}
+
+            {status === apiStatus.inProgress && (
+              <Loader type="ThreeDots" color="#fff" height={50} width={50} />
+            )}
+
+            {status === apiStatus.success && this.getRenderSuccess()}
           </div>
         </div>
       </div>
     )
   }
 }
+
 export default Jobs
